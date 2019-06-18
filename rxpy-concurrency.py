a9ad821006fb3e4c8cb8aad16bdf2444f2997395
Alex Jarnoux <alex.jarnoux@quantium.com.au>
2019-06-18 16:47:11 +1000

    feat: update rx concurrency to use subscribe_on and observe_on correctly

--- rxpy-concurrency.py
+++ rxpy-concurrency.py
@@ -2,7 +2,7 @@ import multiprocessing
 import random
 import time
 
-from rx import of, range, interval
+import rx
 import rx.operators as ops
 from rx.concurrency import ThreadPoolScheduler, ImmediateScheduler, CurrentThreadScheduler, VirtualTimeScheduler, TimeoutScheduler
 import logging
@@ -15,41 +15,39 @@ def intense_calculation(value):
     time.sleep(random.randint(5, 20) * .1)
     return value
 
+# calculate number of CPU's, then create a ThreadPoolScheduler with that number of threads
+optimal_thread_count = multiprocessing.cpu_count()
+pool_scheduler = ThreadPoolScheduler(optimal_thread_count)
 
-if __name__ == "__main__":
-    # calculate number of CPU's, then create a ThreadPoolScheduler with that number of threads
-    optimal_thread_count = multiprocessing.cpu_count()
-    pool_scheduler = ThreadPoolScheduler(optimal_thread_count)
-    imm_scheduler = ImmediateScheduler()
-    ct_scheduler = CurrentThreadScheduler()
-    to_scheduler = TimeoutScheduler()
+# Create Process 1
+rx.of("Alpha", "Beta", "Gamma", "Delta", "Epsilon").pipe(
+    ops.map(lambda s: intense_calculation(s)),
+    ops.subscribe_on(pool_scheduler)
+).subscribe(
+    on_next=lambda s: logging.info(f"PROCESS 1: {s}"),
+    on_error=lambda e: logging.error(e),
+    on_completed=lambda: logging.info("PROCESS 1 done!"),
+    scheduler=pool_scheduler
+)
 
-    # Create Process 1
-    of("Alpha", "Beta", "Gamma", "Delta", "Epsilon").pipe(
-        ops.map(lambda s: intense_calculation(s))
-    ).subscribe_(
-        on_next=lambda s: logging.info(f"PROCESS 1: {s}"),
-        on_error=lambda e: logging.error(e),
-        on_completed=lambda: logging.info("PROCESS 1 done!"),
-        scheduler=pool_scheduler
-    )
+# Create Process 2
+rx.range(1, 10).pipe(
+    ops.map(lambda s: intense_calculation(s)),
+    ops.subscribe_on(pool_scheduler)
+).subscribe(
+    on_next=lambda i: logging.info(f"PROCESS 2: {i}"),
+    on_error=lambda e: logging.error(e),
+    on_completed=lambda: logging.info("PROCESS 2 done!")
+)
 
-    # Create Process 2
-    range(1, 10).pipe(
-        ops.map(lambda s: intense_calculation(s))
-    ).subscribe_(
-        on_next=lambda i: logging.info(f"PROCESS 2: {i}"),
-        on_error=lambda e: logging.error(e),
-        on_completed=lambda: logging.info("PROCESS 2 done!"),
-        scheduler=pool_scheduler
-    )
+# Create Process 3, which is infinite
+rx.interval(1).pipe(
+    ops.map(lambda i: i * 100),
+    ops.observe_on(pool_scheduler),
+    ops.map(lambda s: intense_calculation(s))
+).subscribe(
+    on_next=lambda i: logging.info(f"PROCESS 3: {i}"),
+    on_error=lambda e: logging.error(e)
+)
 
-    # Create Process 3, which is infinite
-    interval(1000).pipe(
-        ops.map(lambda i: i * 100),
-        ops.map(lambda s: intense_calculation(s))
-    ).subscribe_(
-        on_next=lambda i: logging.info(f"PROCESS 3: {i}"),
-        on_error=lambda e: logging.error(e),
-        scheduler=pool_scheduler
-    )
+input("Press any key to exit\n")
