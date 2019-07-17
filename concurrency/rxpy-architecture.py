f5568511c8940684c96e40c869fca289a5d36569
Alex Jarnoux <alex.jarnoux@quantium.com.au>
2019-07-17 15:14:59 +1000

    chore(rxpy): use rx.interval instead of while loop

--- concurrency/rxpy-architecture.py
+++ concurrency/rxpy-architecture.py
@@ -2,6 +2,7 @@ import logging
 import multiprocessing
 import random
 import time
+import traceback
 
 import numpy as np
 
@@ -12,19 +13,21 @@ from rx import operators as ops
 
 logging.basicConfig(level=logging.INFO, format="[%(threadName)s] %(asctime)-15s %(message)s ")
 
+def print_error(message, e):
+    logging.error(f"error: {message}: {e}")
+    logging.error(f"error: {traceback.print_exc()}")
 
 def intense_calculation():
     # sleep for a random short duration between 2.0 to 4.0 seconds
     time.sleep(random.randint(20, 40) * .1)
-    x = np.random.randn(3, 4)
+    # x = np.random.randn(3, 4)
+    x = [random.random(), random.random(), random.random()]
     logging.info(f"out intense_calculation {x}")
     return x
 
-def expand_array(values):
-    logging.info(f"in expand_array values {values}")
-    rows = list()
-    for v in values:
-        rows.append([v - 1, v, v + 1])
+def expand_array(value):
+    logging.info(f"in expand_array values {value}")
+    rows = [value - 1, value, value + 1]
     logging.info(f"out expand_array rows {rows}")
     return rows
 
@@ -36,18 +39,23 @@ stream = Subject()
 stream.pipe(
     ops.observe_on(pool_scheduler),
     ops.do_action(lambda rows: logging.info(f"default {rows}"))
-).subscribe()
+).subscribe(on_error=print_error)
 
 # Flatmapper
 stream.pipe(
-    ops.do_action(lambda row: logging.info(f"flat_map {row}")),
-    ops.flat_map(lambda x: x),
     ops.observe_on(pool_scheduler),
+    ops.flat_map(lambda x: x),
     ops.do_action(lambda row: logging.info(f"flat_map {row}"))
-).subscribe()
+).subscribe(on_error=print_error)
 
-while True:
-    rx.from_(intense_calculation()).pipe(
-        ops.flat_map(expand_array),
-        ops.do_action(stream.on_next)
-    ).subscribe()
+rx.interval(1.0).pipe(
+    ops.observe_on(pool_scheduler),
+    ops.flat_map(lambda _: intense_calculation()),
+    ops.map(expand_array),
+    ops.do_action(stream.on_next)
+).subscribe(
+    on_next=lambda data: logging.info(f"interval {data}"),
+    on_error=print_error
+)
+
+input("Press any key to exit\n")
