769c07f0ac388a2eb7b9368e38e34c8800ad8210
Alex Jarnoux <alex.jarnoux@quantium.com.au>
2019-07-31 13:59:41 +1000

    feat(rxpy): update archetcture to add daemon

--- concurrency/rxpy-architecture.py
+++ concurrency/rxpy-architecture.py
@@ -1,15 +1,15 @@
 import logging
 import multiprocessing
 import random
+import threading
 import time
 import traceback
 
 import numpy as np
-
 import rx
-from rx.subjects import Subject
-from rx.concurrency import ThreadPoolScheduler
 from rx import operators as ops
+from rx.scheduler import ThreadPoolScheduler
+from rx.subject import Subject
 
 logging.basicConfig(level=logging.INFO, format="[%(threadName)s] %(asctime)-15s %(message)s ")
 
@@ -17,13 +17,26 @@ def print_error(message, e):
     logging.error(f"error: {message}: {e}")
     logging.error(f"error: {traceback.print_exc()}")
 
-def intense_calculation():
-    # sleep for a random short duration between 2.0 to 4.0 seconds
-    time.sleep(random.randint(20, 40) * .1)
-    # x = np.random.randn(3, 4)
-    x = [random.random(), random.random(), random.random()]
-    logging.info(f"out intense_calculation {x}")
-    return x
+class CalcService:
+    def __init__(self):
+        self.values = [random.random()]
+        logging.info(f"in CalcService.__init__ {self.values}")
+
+    def get_values_loop(self):
+        while True:
+            x = self.values[:1]
+            x.append(random.random())
+            time.sleep(1)
+            self.values = x
+            logging.info(f"out CalcService.get_values_loop {self.values}")
+            time.sleep(10)
+
+    def fetch_values(self):
+        # sleep for a random short duration between 2.0 to 3.0 seconds
+        time.sleep(random.randint(20, 30) * .1)
+        x = self.values
+        logging.info(f"out CalcService.fetch_values {x}")
+        return x
 
 def expand_array(value):
     logging.info(f"in expand_array values {value}")
@@ -48,9 +61,14 @@ stream.pipe(
     ops.do_action(lambda row: logging.info(f"flat_map {row}"))
 ).subscribe(on_error=print_error)
 
+calc = CalcService()
+
+x = threading.Thread(target=calc.get_values_loop, daemon=True)
+x.start()
+
 rx.interval(1.0).pipe(
     ops.observe_on(pool_scheduler),
-    ops.flat_map(lambda _: intense_calculation()),
+    ops.flat_map(lambda _: calc.fetch_values()),
     ops.map(expand_array),
     ops.do_action(stream.on_next)
 ).subscribe(
